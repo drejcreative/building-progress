f9429752d2088281beb7fe41368ef97b9d1aa3e8
Andrej Vajagic <andrej.v@dreittech.com>
2025-08-28 23:52:37 +0200

    Remove redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,7 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  const redirect = true;
-  if (redirect && pathname === "/coming-soon") {
-    return NextResponse.next();
-  }
-
-  // Redirect all other pages to coming soon
-  return NextResponse.redirect(new URL("/coming-soon", request.url));
+export function middleware() {
+  return NextResponse.next();
 }
 
 export const config = {
