5157ec20f09c1bfadeb27a0853e5211ae056cbf9
Andrej Vajagic <andrej.v@dreittech.com>
2025-11-03 18:30:12 +0100

    Update seo

--- src/app/[locale]/(landing)/presentation/mmontstroj/page.tsx
+++ src/app/[locale]/(landing)/presentation/mmontstroj/page.tsx
@@ -1,4 +1,19 @@
+import { Metadata } from "next";
 import MMontStrojPresentation from "@/components/presentation/mmontstroj/presentation-container";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "MMontStroj Presentation",
+    robots: {
+      index: false,
+      follow: false,
+      googleBot: {
+        index: false,
+        follow: false,
+      },
+    },
+  };
+}
 
 export default function MMontStrojPresentationPage() {
   return <MMontStrojPresentation />;
